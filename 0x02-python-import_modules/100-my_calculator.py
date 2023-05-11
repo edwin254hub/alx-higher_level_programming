6f6d7e81b72a5c2841c3e4952723f08fcea6eae2
edwin254hub <edwinmutunga42@gmail.com>
2023-05-11 09:50:18 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
+    """Handles arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) - 1 != 3:
