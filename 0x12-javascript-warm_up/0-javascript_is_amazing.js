7e092cb204be0eb3c89581a16b1a47fb1319758f
edwin254hub <edwinmutunga42@gmail.com>
2023-08-14 08:28:49 -0700

    javascript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-if (args[0] === undefined)
-{
-  console.log("No argument");
-} else
-{
-  const myVar = 'Javascript is amazing';
-  console.log(myVar);
-}
+
+const myVar = 'Javascript is amazing';
+console.log(myVar);
