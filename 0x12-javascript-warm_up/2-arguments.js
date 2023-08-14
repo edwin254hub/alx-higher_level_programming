ecbcba014708585b1c5a9aee418d6a959fbcaef6
edwin254hub <edwinmutunga42@gmail.com>
2023-08-14 09:00:18 -0700

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-// Check the number of arguments
-const numArguments = process.argv.length - 2;
-
 // Print messages based on the number of arguments
-if (numArguments === 0) {
+if (process.argv.length === 0) {
   console.log("No argument");
-} else if (numArguments === 1) {
+} else if (process.argv.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
 }
-
